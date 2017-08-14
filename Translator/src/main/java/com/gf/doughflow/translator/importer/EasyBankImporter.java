3f7e1b5e8f57ee8a038c35e6208bbb77b5006ca9
gix <gilbert.fritz@gmx.at>
2017-08-15 00:39:24 +0200

    fixed message with quotes bug

--- Translator/src/main/java/com/gf/doughflow/translator/importer/EasyBankImporter.java
+++ Translator/src/main/java/com/gf/doughflow/translator/importer/EasyBankImporter.java
@@ -8,15 +8,15 @@ public class EasyBankImporter implements IImporter {
 
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
     private final int accountId;
-    
-    public EasyBankImporter(int accountId){
+
+    public EasyBankImporter(int accountId) {
         this.accountId = accountId;
     }
-    
+
     @Override
-    public Transaction toTransaction(Object record) throws Exception{
-        String[] recordSplit = ((String)record).split(";");
-        if(recordSplit.length != 6){
+    public Transaction toTransaction(Object record) throws Exception {
+        String[] recordSplit = ((String) record).split(";");
+        if (recordSplit.length != 6) {
             throw new Exception("Invalid record: " + record);
         }
         Transaction t = new Transaction();
@@ -26,20 +26,28 @@ public class EasyBankImporter implements IImporter {
         t.setDate(sdf.parse(recordSplit[3]));
         return t;
     }
-    
-    private String convertDescription(String description){
-        String ret = description;
-        while( ret.contains("  ")){
-            ret = ret.replaceAll("  ", " ");
-        }
-        return replaceGermanLetters(ret);
+
+    private String convertDescription(String description) {
+        return replaceGermanLetters(removeSpaces(removeQuotes(description)));
+    }
+
+    private String removeQuotes(String input){
+        return input.replaceAll("\"", "");
     }
     
+    private String removeSpaces(String input) {
+        String ret = input;
+        while (ret.contains("  ")) {
+            ret = ret.replaceAll("  ", " ");
+        }
+        return ret;
+    }
+
     private String replaceGermanLetters(String description) {
         return description.toLowerCase().replaceAll("ä", "ae").replaceAll("ö", "oe").replaceAll("ü", "ue").replaceAll("ß", "ss");
     }
-    
-    private Double convertToDouble(String ds){
+
+    private Double convertToDouble(String ds) {
         String dsPeriod = ds.replaceAll("\\.", "").replaceAll(",", ".");
         return Double.valueOf(dsPeriod);
     }
